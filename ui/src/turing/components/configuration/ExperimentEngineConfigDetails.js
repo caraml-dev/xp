0cc7359931f9f6c031645d6d6ecff8cb8000a613
Terence Lim <terencelimxp@gmail.com>
2022-06-13 08:15:07 -0700

    Add missing context providers

--- ui/src/turing/components/configuration/ExperimentEngineConfigDetails.js
+++ ui/src/turing/components/configuration/ExperimentEngineConfigDetails.js
@@ -3,26 +3,29 @@ import React from "react";
 import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 
 import { ConfigSectionPanel } from "components/config_section/ConfigSectionPanel";
+import { ConfigProvider } from "config";
 
 import { ExperimentsConfigGroup } from "./experiments_config/ExperimentsConfigGroup";
 import { VariablesConfigGroup } from "./variables_config/VariablesConfigGroup";
 
 const ExperimentEngineConfigDetails = ({ projectId, config }) => (
-  <EuiFlexGroup direction="row" wrap>
-    <EuiFlexItem grow={1} className="euiFlexItem--smallPanel">
-      <ConfigSectionPanel title="Summary" className="experimentSummaryPanel">
-        <ExperimentsConfigGroup projectId={projectId} />
-      </ConfigSectionPanel>
-    </EuiFlexItem>
+  <ConfigProvider>
+    <EuiFlexGroup direction="row" wrap>
+      <EuiFlexItem grow={1} className="euiFlexItem--smallPanel">
+        <ConfigSectionPanel title="Summary" className="experimentSummaryPanel">
+          <ExperimentsConfigGroup projectId={projectId} />
+        </ConfigSectionPanel>
+      </EuiFlexItem>
 
-    <EuiFlexItem grow={2} className="euiFlexItem--smallPanel">
-      <ConfigSectionPanel
-        title="Variables"
-        className="experimentVariablesPanel">
-        <VariablesConfigGroup variables={config.variables} />
-      </ConfigSectionPanel>
-    </EuiFlexItem>
-  </EuiFlexGroup>
+      <EuiFlexItem grow={2} className="euiFlexItem--smallPanel">
+        <ConfigSectionPanel
+          title="Variables"
+          className="experimentVariablesPanel">
+          <VariablesConfigGroup variables={config.variables} />
+        </ConfigSectionPanel>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  </ConfigProvider>
 );
 
 export default ExperimentEngineConfigDetails;
