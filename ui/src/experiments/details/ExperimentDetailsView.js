e176f6752775746647691afc7f10002c7c8fa72f
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2022-09-20 18:02:57 +0800

    Address PR comments - UI formatting

--- ui/src/experiments/details/ExperimentDetailsView.js
+++ ui/src/experiments/details/ExperimentDetailsView.js
@@ -129,7 +129,7 @@ const ExperimentDetailsView = ({ projectId, experimentId, ...props }) => {
         </Fragment>
       )}
       <EuiSpacer size="l" />
-    </EuiPageTemplate >
+    </EuiPageTemplate>
   );
 };
 
