7dc089e9387477815d54327846eedbb9238866d5
ewezy <ziyi.ewe@gojek.com>
2022-09-23 12:24:04 +0800

    Replace colour hexes corresponding to subdued to default

--- ui/src/services/experiment/ExperimentStatus.js
+++ ui/src/services/experiment/ExperimentStatus.js
@@ -13,7 +13,7 @@ export const getExperimentStatus = (experiment) => {
   const statusMapping = {
     Deactivated: {
       label: "Deactivated",
-      color: "#6A717D",
+      color: "default",
       iconType: "cross",
     },
     Completed: {
