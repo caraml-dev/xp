5c277dca79ca0b89422cea38bdc671a7a4522d7e
ewezy <ziyi.ewe@gojek.com>
2022-10-26 16:35:31 +0800

    Add test set up to create mock google credentials

--- plugins/turing/manager/experiment_manager_test.go
+++ plugins/turing/manager/experiment_manager_test.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
@@ -22,6 +23,9 @@ func TestNewExperimentManagerImplementsCustomExperimentManagerInterface(t *testi
 }
 
 func TestNewExperimentManager(t *testing.T) {
+	reset := testSetupEnvForGoogleCredentials(t)
+	defer reset()
+
 	// Define test cases
 	tests := map[string]struct {
 		input json.RawMessage
@@ -298,5 +302,49 @@ func TestValidateExperimentConfig(t *testing.T) {
 				assert.Nil(t, err)
 			}
 		})
+	}
+}
+
+// testSetupEnvForGoogleCredentials creates a temporary file containing dummy service account JSON
+// then set the environment variable GOOGLE_APPLICATION_CREDENTIALS to point to the file.
+// This is useful for tests that assume Google Cloud Client libraries can automatically find
+// the service account credentials in any environment.
+// At the end of the test, the returned function can be called to perform cleanup.
+func testSetupEnvForGoogleCredentials(t *testing.T) (reset func()) {
+	serviceAccountKey := []byte(`{
+		"type": "service_account",
+		"project_id": "foo",
+		"private_key_id": "bar",
+		"private_key": "baz",
+		"client_email": "foo@example.com",
+		"client_id": "bar_client_id",
+		"auth_uri": "https://oauth2.googleapis.com/auth",
+		"token_uri": "https://oauth2.googleapis.com/token"
+	}`)
+
+	file, err := os.CreateTemp("", "dummy-service-account")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.WriteFile(file.Name(), serviceAccountKey, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", file.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return func() {
+		err := os.Unsetenv("GOOGLE_APPLICATION_CREDENTIALS")
+		if err != nil {
+			t.Log("Cleanup failed", err)
+		}
+		err = os.Remove(file.Name())
+		if err != nil {
+			t.Log("Cleanup failed", err)
+		}
 	}
 }
